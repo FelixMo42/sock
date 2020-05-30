c67d90af9beb434471719fea9b06e0f9894b808f
FelixMo42 <felix@tacocat.com>
2020-05-29 23:16:34 -0700

    fixed step bug

--- move.js
+++ move.js
@@ -1,5 +1,5 @@
 const Vector = (x, y) => ([x, y])
-const Player = (p, v, s=Math.random()) => ({p, v, s})
+const Player = (p, v) => ({p, v})
 
 // 
 
@@ -7,24 +7,31 @@ const GetPlayer = (players, [x, y]) => players.find( player => player.p[0] == x 
 const HasPlayer = (players, pos) => GetPlayer( players, pos ) != undefined
 
 const Step = ([x0, y0], [x1, y1], callback) => {
-    let dx = x1 - x0
-    let dy = y1 - y0
+    let dx = Math.abs(x1 - x0)
+    let dy = Math.abs(y1 - y0)
+    let sx = Math.sign(x1 - x0)
+    let sy = Math.sign(y1 - y0)
 
-    if (Math.abs(dx) > Math.abs(dy)) {
-        for (let x = 0; x <= Math.abs(dx); x++) {
-            if ( callback([ x0 + x, y0 + dy / dx * x ]) ) {
-                return [ x0 + x, y0 + dy / dx * x ] 
-            }
-        }
+    if (dx > dy) for (let x = 0; x <= dx; x++) {
+        let pos = [
+            x0 + sx * x,
+            y0 + dy / dx * sx * x 
+        ]
+
+        console.log(pos)
+
+        if ( callback( pos ) ) return pos
     }
     
-    if (Math.abs(dx) < Math.abs(dy)) {
-        for (let y = 0; y <= Math.abs(dy); y++) {
-            console.log( y0 + y )
-            if ( callback([ x0 + dx / dy * y, y0 + y ]) ) {
-                return [ x0 + dx / dy * y, y0 + y ] 
-            }
-        }
+    if (dy != 0 && dx != 0) for (let y = 0; y <= dy; y++) {
+        let pos = [
+            x0 + dx / dy * sy * y,
+            y0 + sy * y
+        ]
+
+        console.log(pos)
+
+        if ( callback(pos) ) return pos
     }
 
     return [x1, y1]
@@ -38,17 +45,13 @@ const Tick = (players) => {
 
 const Move = (players, player) => {
     // make sure their isent a player where were trying to do
-    if ( !HasPlayer( players, player.v ) ) {
-        player.p = player.v
-    } else {
-        Step( player.v, player.p, pos => {
-            if ( !HasPlayer(players, pos) ) {
-                player.p = pos
+    Step( player.v, player.p, pos => {
+        if ( !HasPlayer(players, pos) ) {
+            player.p = pos
 
-                return true
-            }
-        } )
-    }
+            return true
+        }
+    } )
 
     // reset the movement vector of the player
     player.v = player.p
@@ -63,7 +66,7 @@ const Draw = (players) => {
 // run it
 
 let players = [
-    Player( [4, 0], [4, 4] ),
+    Player( [8, 0], [4, 4] ),
     Player( [4, 4], [4, 4] )
 ]
 
